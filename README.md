4ad705a71af29a294b9e2ef314cc37e51cf3f930
JIM MOODY <jim.moody@libertymutual.com>
2017-12-01 14:55:58 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Styled hint blocks in your docs
+
+This plugins requires gitbook `>=2.0.0`.
+
+## Install
+
+Add the below to your `book.json` file, then run `gitbook install` :
+
+```json
+{
+  "plugins": ["styled-blockquotes"]
+}
+```
+
+## Usage
+
+You can now provide styled blockquotes using the following syntax
+
+```md
+> **Info**
+> content
+```
+
+Replace "Info" with any of the available styles to change the style
+
+Available styles are:
+
+* `info` (default)
+* `tip`
+* `danger`
+* `working`
+
+The above example will produce a styled alert, with an icon:
+
+```html
+<div class="alert alert-info hints-alert">
+  <div class="hints-icon"><i class="fa fa-info"></i></div>
+  <div class="hints-container">
+    <p>Important info: this note needs to be highlighted</p>
+  </div>
+</div>
+```
+
+
+## Acknowledgements
+
+This project borrowed heavily from the following plugins:
+
+* [Richquotes Plugin](https://github.com/erixtekila/gitbook-plugin-richquotes)
+* [Hints Plugin](https://github.com/GitbookIO/plugin-hints)
---
+++ index.js
@@ -0,0 +1,55 @@
+var cheerio = require('cheerio')
+
+const icons = {
+  info: 'fa fa-info-circle',
+  tip: 'fa fa-mortar-board',
+  danger: 'fa fa-exclamation-circle',
+  working: 'fa fa-wrench'
+}
+
+const getStyle = $blockquote => {
+  const $strong = $blockquote.find('p:first-child > strong:first-child')
+  // if there is no strong tag then the user has not defined a style
+  if (!$strong || $strong.length === 0) {
+    return
+  }
+  // get the user defined style
+  return $strong.text().toLowerCase()
+}
+const generateBlockquoteHtml = ({ style, icon, content }) => {
+  return `<div class="alert alert-${style} hints-alert">
+		<div class="hints-icon"><i class="fa ${icon}"></i></div>
+		<div class="hints-container">${content}</div>
+		</div>`
+}
+module.exports = {
+  book: {
+    assets: './assets',
+    css: ['plugin-styled-blockquotes.css']
+  },
+  hooks: {
+    page: page => {
+      const $ = cheerio.load(page.content)
+
+      $('blockquote').each((i, blockquote) => {
+        // define the cheerio elements
+        const $blockquote = $(blockquote)
+
+        // define elements needed for the blockquote html
+        const style = getStyle($blockquote) || 'info'
+        const content = $blockquote.text()
+        const icon = icons[style]
+
+        const blockquoteHtml = generateBlockquoteHtml({ style, icon, content })
+
+        // append the new blockquote (as a div) to the parent
+        $blockquote.parent().append(blockquoteHtml)
+        // remove the old blockquote tag, so we dont get the default styling
+        $blockquote.remove()
+        // update the page content html with the new html
+        page.content = $.html()
+      })
+      return page
+    }
+  }
+}
